52fdddee961403533c8d43c912a774a709442a02
Avi Greenbury <ialoneambest@gmail.com>
2011-04-11 23:14:22 +0100

    this is all the documentation there is so far

---
+++ test.pl
@@ -0,0 +1,41 @@
+#! /usr/bin/perl
+
+use Vpostmail;
+use 5.010;
+
+my $d = Vpostmail->new();
+
+print "Number of users on the system:";
+say $d->numUsers();
+
+print "Number of domains on this system:";
+say $d->numDomains();
+
+say "List of domains on this system:";
+my @domains = $d->listDomains();
+foreach(@domains){
+	print "\t$_\n";
+}
+
+say "list of all users on the system";
+my @users = $d->listUsers();
+foreach(@users){
+	print "\t$_\n";
+}
+
+
+
+print "Set domain to avi.co.\n";
+$d->setDomain('avi.co');
+
+print "Number of users on domain set with \$d->setDomain:";
+say $d->numUsers;
+
+print "Users on domain:\n";
+my @users = $d->listUsers();
+foreach(@users){
+	print "\t$_\n";
+}
+
+
+print "\n";
