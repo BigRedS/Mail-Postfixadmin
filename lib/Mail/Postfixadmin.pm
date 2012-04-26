531a81208a83c86209f661f1feeaee770fc72567
BigRedS <ialoneambest@gmail.com>
2012-04-26 17:44:30 +0100

    No pound symbols in passwords and removed an extraneous print

--- lib/Mail/Postfixadmin.pm
+++ lib/Mail/Postfixadmin.pm
@@ -1477,7 +1477,7 @@ sub _dbSelect {
 	while(my $row = $sth->fetchrow_hashref){
 		push(@return, $row);
 	}
-	print "\n\n<$query>\n\n";
+#	print "\n\n<$query>\n\n";
 	return @return;
 }
 
@@ -1494,11 +1494,11 @@ sub _mysqlNow() {
 sub generatePassword() {
 	my $self = shift;
 	my $length = shift;
-	print $length."\n";
+#	print $length."\n";
 	my @characters = qw/a b c d e f g h i j k l m n o p q r s t u v w x y z
 			    A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 
 			    1 2 3 4 5 6 7 8 9 0 - =
-			    ! " £ $ % ^ & * ( ) _ +
+			    ! " $ % ^ & * ( ) _ +
 			    [ ] ; # , . : @ ~ < > ?
 			  /;
 	my $password;
