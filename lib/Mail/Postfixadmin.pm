0d1738c4a1571ed30b2ae463b6ad32d5c2f09b89
BigRedS <ialoneambest@gmail.com>
2012-08-16 16:23:26 +0100

    generatePassword() checks it's passed a numeric argument. Accepts decimal fractions for no good reason

--- lib/Mail/Postfixadmin.pm
+++ lib/Mail/Postfixadmin.pm
@@ -1722,6 +1722,8 @@ sub _mysqlNow() {
 sub generatePassword() {
 	my $self = shift;
 	my $length = shift;
+	Carp::croak "generatePassword() called with no arguments (length required)" if $length =~ /^$/;
+	Carp::croak "generatePassword() called with non-numeric argument (length expected)" if $length !~ /^\s*\d+\.?\d*\s*$/;
 	my @characters = qw/a b c d e f g h i j k l m n o p q r s t u v w x y z
 			    A B C D E F G H I J K L M N O P Q R S T U V W X Y Z 
 			    1 2 3 4 5 6 7 8 9 0 - =
